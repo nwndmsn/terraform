16952a870a840aac3bfc584afcd00d41144215ab
sje <nwndmsn@naver.com>
2021-10-29 14:27:29 +0900

    sg-1.step10

--- 10_sg.tf
+++ 10_sg.tf
@@ -36,6 +36,17 @@ resource "aws_security_group" "sje_websg" {
       security_groups  =  null
       prefix_list_ids  =  null
       self             =  null
+    },
+    {
+      description     = "mysql"
+      from_port       = 3306
+      to_port         = 3306
+      protocol        = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks  = ["::/0"]
+      security_groups  =  null
+      prefix_list_ids  =  null
+      self             =  null
     }
   ]
 
